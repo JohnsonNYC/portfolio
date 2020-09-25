278d23d749e1b24d3b2c10326bba89896864dd71
JohnsonNYC <jkow95@gmail.com>
2020-09-25 18:28:46 -0400

    added photo fro banner of homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar'
-
+import './Home.css'
 const Home = () => {
     return (
-        <div>
-            <Navbar />
+        <div className='home-wrapper'>
+            <div className='banner'>
+                <Navbar />
+                <div className='description-wrapper'>
+                        {/* try to get description 1 to be of font family Bebas Neue Pro */}
+                        <p id='description-1'><strong> Hello I'm Johnson Kow </strong></p>
+                        <p>For the past year, I've been honing my skills as a fullstack developer.</p>
+                        <p></p>
+                </div>
+
+            </div>
         </div>
     );
 }
