ca0a3a3f551356406db37377a75e0bbe1eb89438
JohnsonNYC <jkow95@gmail.com>
2020-09-25 20:59:12 -0400

    Corrected the background size. This part of the page is finalized

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import './HomeBanner.css'
+import './HomeBanner.css';
 
 const HomeBanner = () => {
     return (
         <div className='description-wrapper'>
             {/* try to get description 1 to be of font family Bebas Neue Pro */}
             <p id='description-1'><strong> Hello I'm Johnson Kow </strong></p>
-            <p>For the past year, I've been honing my skills as a fullstack developer.</p>
-            <p> Add more information here yaata yaata</p>
+            <p className='description'> For the past year, I've been honing my skills as a fullstack developer.</p>
+            <p className='description'> Add more information here yaata yaata</p>
         </div>
     );
 }
