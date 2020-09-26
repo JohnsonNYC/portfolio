c59017a61987b0d257bafc677599a29ea29ebd72
JohnsonNYC <jkow95@gmail.com>
2020-09-25 20:26:16 -0400

    completed the home page banner element layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Navbar from './Navbar'
+import HomeBanner from './HomeBanner'
 import './Home.css'
+
 const Home = () => {
     return (
         <div className='home-wrapper'>
-            <div className='banner'>
                 <Navbar />
-                <div className='description-wrapper'>
-                        {/* try to get description 1 to be of font family Bebas Neue Pro */}
-                        <p id='description-1'><strong> Hello I'm Johnson Kow </strong></p>
-                        <p>For the past year, I've been honing my skills as a fullstack developer.</p>
-                        <p></p>
-                </div>
-
+            <div className='banner'>
+                <HomeBanner />
             </div>
         </div>
     );
---
+++ src/components/HomeBanner.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './HomeBanner.css'
+
+const HomeBanner = () => {
+    return (
+        <div className='description-wrapper'>
+            {/* try to get description 1 to be of font family Bebas Neue Pro */}
+            <p id='description-1'><strong> Hello I'm Johnson Kow </strong></p>
+            <p>For the past year, I've been honing my skills as a fullstack developer.</p>
+            <p> Add more information here yaata yaata</p>
+        </div>
+    );
+}
+
+export default HomeBanner;
