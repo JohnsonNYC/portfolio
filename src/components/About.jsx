439ce05f072dce0d6db6aa858dfcca4ffee00911
JohnsonNYC <jkow95@gmail.com>
2020-09-26 03:19:29 -0400

    finalized the layout, fonts, and image sizing for the about section of homepage

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'fontsource-archivo-narrow'
 import './About.css'
 
 const About = () => {
@@ -10,7 +11,7 @@ const About = () => {
         
             <div className='about-text-picture'>
                 <div className='about-text-container'>
-                    <p className='about-text'>Sorelle Amore is a well-known photographer, filmmaker, influencer, and savvy businesswoman who inspires her millions of followers around the globe on how to maximise the human experience and live a life of wonder.</p>
+                    <p className='about-text'> GET ARCHIVO NARROW TO WORK is a well-known photographer, filmmaker, influencer, and savvy businesswoman who inspires her millions of followers around the globe on how to maximise the human experience and live a life of wonder.</p>
                     <p className='about-text'>With the motto “Beautifully Building Better Beings”, Sorelle is a recognisable global media personality who interweaves art with informative content. Reaching an audience of over 1.5 million followers mainly on the YouTube and Instagram platforms, Sorelle is mostly known for creating messaging around the subjects of travel, photography and human optimisation.</p>
                     <p className='about-text'>Having worked with international brands including Samsung, Porsche, Squarespace, Armani, and Skillshare, Sorelle has extensive experience partnering with companies to successfully promote their message to a broad range of audiences, in many regions of the world.</p>
                     <p className='about-text'>In 2017, Sorelle rose to the top of the global “Best Job On The Planet” competition, beating 17,000+ other influencers to win the five-figure cash prize, three months of luxury travel, and a two year position as the ambassador for ThirdHom</p>
