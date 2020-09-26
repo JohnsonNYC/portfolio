0f3a0a94a6c669291f4647dd6cc81d2d6957236c
JohnsonNYC <jkow95@gmail.com>
2020-09-26 01:54:22 -0400

    Currently adding the about me inside of the homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import Navbar from './Navbar'
 import HomeBanner from './HomeBanner'
+import About from './About'
 import './Home.css'
 
 const Home = () => {
     return (
-        <div className='home-wrapper'>
+        <div className='home-page'>
+            <div className='banner-wrapper'>
                 <Navbar />
-            <div className='banner'>
-                <HomeBanner />
+                <div className='banner'>
+                    <HomeBanner />
+                </div>
             </div>
+            <About />
         </div>
     );
 }
