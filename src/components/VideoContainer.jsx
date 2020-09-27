b6cd81d160c07029953234dcac7e314747ba4625
JohnsonNYC <jkow95@gmail.com>
2020-09-27 18:53:25 -0400

    Added second project video

--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -5,13 +5,18 @@ const VideoContainer = () => {
         <div className='video-container'>
             <div id='blog-header' className='subtitle'> Projects </div>
             <iframe
-                title ="unique ID"
+                title="unique ID"
                 src="https://www.youtube.com/embed/Bow4BPYAJlg"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen 
+                allowfullscreen
             />
-            
+            <iframe title='Travel Rails'
+                src="https://www.youtube.com/embed/AUwPwo0MrgE"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+            />
 
         </div>
     );
