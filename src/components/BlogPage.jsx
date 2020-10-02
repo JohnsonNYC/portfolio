bfaf48cdca1006a78e48e49510ee41fbd2ecedcd
JohnsonNYC <jkow95@gmail.com>
2020-10-01 20:31:39 -0400

    Started Blog Page Component

---
+++ src/components/BlogPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Navbar'
+
+const BlogPage = () => {
+    return (
+        <div id='blog-page'>
+            <div id='blog-banner'>
+                <Navbar /> 
+            </div>
+            
+        </div>
+    );
+}
+
+export default BlogPage;
