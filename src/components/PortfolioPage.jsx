e5f6f2c516a74f24549d3433307f79edfd53120a
JohnsonNYC <jkow95@gmail.com>
2020-10-03 18:50:19 -0400

    Created Banner for the Portfolio Page

--- src/components/PortfolioPage.jsx
+++ src/components/PortfolioPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Navbar from './Navbar'
 import './PortfolioPage.css'
 
 const PortfolioPage = () => {
     return ( 
-        <div>
-            
+        <div id='navbar-page'>
+            <div id='portfolio-banner'>
+                <Navbar/>
+            </div>
+            <div> Insert Portfolio Info in here</div>
         </div>
     );
 }
