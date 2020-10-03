3d980cdcd97905aeceb46c949613d2e8af1801d6
JohnsonNYC <jkow95@gmail.com>
2020-10-03 15:41:44 -0400

    Configured Colors of links in about page and added link to twitter

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,10 +21,10 @@ const About = () => {
                 <img className='portrait' src={require("../png/Johnson-01378.jpg")} alt='Portrait' />
             </div>
             <div className='social-media-handles'>
-                <p> LinkedIn: <a href="https://www.linkedin.com/in/johnson-kow/">@johnson-kow</a> </p>
-                <p> GitHub: <a href="https://github.com/JohnsonNYC">@JohnsonNYC</a></p>
-                <p> Twitter: <a href=" ">@TheJohnsonKow</a></p>
-                <p> Medium: <a href=" ">@jkow</a></p>
+                <p> LinkedIn: <a className='handles' href="https://www.linkedin.com/in/johnson-kow/">@johnson-kow</a> </p>
+                <p> GitHub: <a   className='handles' href="https://github.com/JohnsonNYC">@JohnsonNYC</a></p>
+                <p> Twitter: <a  className='handles' href="https://twitter.com/TheJohnsonKow">@TheJohnsonKow</a></p>
+                <p> Medium: <a   className='handles' href=" ">@jkow</a></p>
             </div>
         </div>
     );
