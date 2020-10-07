8ca07f8ccdeb3b7fe4926f7be378da1f17cf81c6
JohnsonNYC <jkow95@gmail.com>
2020-10-07 18:23:18 -0400

    Creating the layout for each project inside of porfolio page

--- src/components/PortfolioPage.jsx
+++ src/components/PortfolioPage.jsx
@@ -3,7 +3,7 @@ import Navbar from './Navbar'
 import './PortfolioPage.css'
 
 const PortfolioPage = () => {
-    
+
     return (
         <div id='navbar-page'>
             <div id='portfolio-banner'>
@@ -11,30 +11,41 @@ const PortfolioPage = () => {
             </div>
             <div className='video-container'>
                 <div id='blog-header' className='subtitle'> Projects </div>
-                <iframe
-                    title="Picology"
-                    src="https://www.youtube.com/embed/Bow4BPYAJlg"
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                />
-                <iframe
-                    title='Travel Rails'
-                    src="https://www.youtube.com/embed/AUwPwo0MrgE"
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                />
-                <iframe
-                    title='Study Beans'
-                    src="https://www.youtube.com/embed/UjEURK5LSIo"
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                />
+                <div className='project-summary'>
+                    <iframe
+                        title="Picology"
+                        src="https://www.youtube.com/embed/Bow4BPYAJlg"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                    />
+                    <p>Description 1</p>
+                </div>
+                <div className='project-summary'>
+                    <iframe
+                        title='Travel Rails'
+                        src="https://www.youtube.com/embed/AUwPwo0MrgE"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                    />
+                    <p> Description 2 </p>
+                </div>
+
+                <div className='project-summary'>
+                    <iframe
+                        title='Study Beans'
+                        src="https://www.youtube.com/embed/UjEURK5LSIo"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                    />
+                    <p> Description 3 </p>
+                </div>
             </div>
         </div>
     );
 }
 
 export default PortfolioPage;
+
