c3437c39a306aabe62d971dfd5bacc2ea58a5000
JohnsonNYC <jkow95@gmail.com>
2020-11-02 12:03:48 -0500

    intialized state for the contact form in contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 import './Contact.css'
 
 const Contact = () => {
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [ email, setEmail] = useState("")
+    const [message, setMessage] = useState("")
     return ( 
         <div className='PlaceHolder'> This page is currenlty being worked on.</div>
     );
