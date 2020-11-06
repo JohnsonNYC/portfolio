d5e93e18c17ff1a2ff16792dd40b717c9411bf6c
JohnsonNYC <jkow95@gmail.com>
2020-11-06 15:13:35 -0500

    Attempt to Lazy Load

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import {Switch, Route} from 'react-router-dom'
 import './App.css';
-import HomePage from './components/HomePage'
+
+// import HomePage from './components/HomePage'
 import AboutPage from './components/AboutPage'
 import BlogPage from './components/BlogPage'
 import PortfolioPage from './components/PortfolioPage'
 import Contact from './components/Contact'
 
+const HomePage = React.lazy(() => import("./components/HomePage"))
+const LoadingPage = () => <div>Loading Page</div>
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route path="/about" render={()=> <AboutPage/>}/>
-        <Route path="/portfolio" render={()=> <PortfolioPage/>}/>
-        <Route path="/blog" render={()=> <BlogPage />} />
-        <Route path="/contact" render={()=> <Contact />} />
-        <Route exact path="/" component={HomePage}/>
-      </Switch>
+      <Suspense fallback={<LoadingPage />}>
+        <Switch>
+          <Route path="/about" render={()=> <AboutPage/>}/>
+          <Route path="/portfolio" render={()=> <PortfolioPage/>}/>
+          <Route path="/blog" render={()=> <BlogPage />} />
+          <Route path="/contact" render={()=> <Contact />} />
+          <Route exact path="/" component={HomePage}/>
+        </Switch>
+      </Suspense>
     </div>
   );
 }
