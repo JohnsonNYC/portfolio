ff580a1b11a3e780628c1e861e218fbfaeb134b2
JohnsonNYC <jkow95@gmail.com>
2021-01-03 14:49:02 -0500

    removed Resume tab. PDF Fails to load on deployment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ class Navbar extends Component {
                         <div className='navButton' id='nb-2'><Link className='navLink' to='/about'>About</Link></div>
                         <div className='navButton' id='nb-3'><Link className='navLink' to='/portfolio'>Portfolio</Link></div>
                         <div className='navButton' id='nb-4'><Link className='navLink' to='/blog'>Blog</Link></div>
-                        <div className='navButton' id='nb-5'><Link className='navLink' to='/resume'> Resume </Link></div>
+                        {/* <div className='navButton' id='nb-5'><Link className='navLink' to='/resume'> Resume </Link></div> */}
 
                         <div href={void(0)} className="icon" onClick={this.tabletResponsive}>
                             <i className="fa fa-bars"></i>
