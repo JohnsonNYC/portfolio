b841978ab33d3603d855809fc790be0297a34e9c
JohnsonNYC <jkow95@gmail.com>
2021-05-12 10:54:55 -0400

    updated contact sheet

--- gatsby-starter-strata/src/pages/index.js
+++ gatsby-starter-strata/src/pages/index.js
@@ -57,9 +57,7 @@ const HomeIndex = () => {
         <section id="three">
           <h2>Get In Touch</h2>
           <p>
-            Accumsan pellentesque commodo blandit enim arcu non at amet id arcu
-            magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem
-            vulputate lorem neque lorem ipsum dolor.
+            Plese provide your contact information along with you message and I will get back to you. Thanks!  
           </p>
           <div className="row">
             <div className="8u 12u$(small)">
@@ -103,9 +101,7 @@ const HomeIndex = () => {
                   <h3 className="icon fa-home">
                     <span className="label">Address</span>
                   </h3>
-                  1234 Somewhere Rd.
-                  <br />
-                  Nashville, TN 00000
+                  New York, NY
                   <br />
                   United States
                 </li>
@@ -113,13 +109,13 @@ const HomeIndex = () => {
                   <h3 className="icon fa-mobile">
                     <span className="label">Phone</span>
                   </h3>
-                  000-000-0000
+                  646-954-9776
                 </li>
                 <li>
                   <h3 className="icon fa-envelope-o">
                     <span className="label">Email</span>
                   </h3>
-                  <a href="#">hello@untitled.tld</a>
+                  <a href="#">jkow95@gmail.com</a>
                 </li>
               </ul>
             </div>
