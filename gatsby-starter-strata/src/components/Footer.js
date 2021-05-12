6960903b1e8147e6550ba6edc1bf17c5e6e7f724
JohnsonNYC <jkow95@gmail.com>
2021-05-12 10:49:57 -0400

    Updated Relevent Projects

---
+++ gatsby-starter-strata/src/components/Footer.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Footer = () => (
+  <div id="footer">
+    <div className="inner">
+      <ul className="icons">
+        <li>
+          <a href="https://twitter.com/TheJohnsonKow" className="icon fa-twitter">
+            <span className="label">Twitter</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/JohnsonNYC" className="icon fa-github">
+            <span className="label">Github</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://johnsonkow.medium.com/" className="icon fa-medium">
+            <span className="label">Medium</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className="icon fa-youtube-play">
+            <span className="label">Youtube</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+)
+
+export default Footer
---
+++ gatsby-starter-strata/src/components/Gallery/constants/defaultImages.js
@@ -0,0 +1,38 @@
+import full10 from '../../../assets/images/fulls/10.png'
+import thumb10 from '../../../assets/images/thumbs/10.png'
+import full11 from '../../../assets/images/fulls/11.png'
+import thumb11 from '../../../assets/images/thumbs/11.png'
+import full12 from '../../../assets/images/fulls/12.png'
+import thumb12 from '../../../assets/images/thumbs/12.png'
+
+
+export const DEFAULT_IMAGES = [
+    {
+      id: '1',
+      source: full10,
+      thumbnail: thumb10,
+      caption: 'Picology',
+      description: 'An online ecommerce website for users to buy photographic prints from their favorite photographers.',
+    },
+    {
+      id: '2',
+      source: full11,
+      thumbnail: thumb11,
+      caption: 'Travel Rails',
+      description: 'An AirBnb copycat project where a user can either reserve a listing or post a listing.',
+    },
+    {
+      id: '3',
+      source: full12,
+      thumbnail: thumb12,
+      caption: 'Study Beans',
+      description: 'Personal project where users can find workspace availability inside of cafes aronud New York City.',
+    },
+    // {
+    //   id: '4',
+    //   source: full04,
+    //   thumbnail: thumb04,
+    //   caption: 'Photo 4',
+    //   description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
+    // },
+]
